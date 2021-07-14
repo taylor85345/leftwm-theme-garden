e97f5db15e8ac4ee306698b684365f4fbe70ff6d
taylor85345 <taylor85345@hotmail.com>
2021-07-14 01:58:26 -0700

    adding some relevant files for use outside of this config

---
+++ misc/default_apps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#This file should be placed in the root of your leftwm config ($HOME/.config/leftwm)
+
+#Define default applications to be sourced by default_apps
+TERM=kitty          #Tag 1
+BROWSER=brave       #Tag 2
+DOC=libreoffice     #Tag 3
+FILE=dolphin        #Tag 4
+PLAYER=vlc          #Tag 5
+GAME=steam          #Tag 6
+PHOTO=krita         #Tag 7
+SANDBOX=virtualbox  #Tag 8
+DEV="kitty -e nvim" #Tag 9
